6b39c975598e524aff69918db7b216c2e49a53fd
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2023-11-29 09:36:57 +0800

    Replaced Resume

--- Resume/javascript.js
+++ Resume/javascript.js
@@ -1,17 +1,18 @@
-$('.skills-prog li .skills-bar').each(function(i) {
-    var bar = $(this).find('.bar');
-    bar.delay(i * 150).animate({
+$('.skills-prog li').find('.skills-bar').each(function(i) {
+
+    $(this).find('.bar').delay(i * 150).animate({
         width: $(this).parents().attr('data-percent') + '%'
     }, 1000, 'linear', function() {
-        bar.css('transition-duration', '.5s');
+
+        $(this).css('transition-duration', '.5s');
     });
 });
 
-$('.skills-soft li svg').each(function(i) {
+$('.skills-soft li').find('svg').each(function(i) {
     var circle = $(this).children('.cbar');
     var r = circle.attr('r');
     var c = Math.PI * (r * 2);
-    var percent = $(this).parent().data('percent');
+    var percent = $(this).data('percentage.chart');
     var cbar = ((100 - percent) / 100) * c;
 
     circle.css({
@@ -20,19 +21,17 @@ $('.skills-soft li svg').each(function(i) {
     });
 
     circle.delay(i * 150).animate({
-        'stroke-dashoffset': cbar
+        strokeDashoffset: cbar
     }, 1000, 'linear', function() {
         circle.css('transition-duration', '.3s');
     });
 
-    var small = $(this).siblings('small');
-    small.prop('Counter', 0).delay(i * 150).animate({
+    $(this).siblings('small').prop('Counter', 0).delay(i * 150).animate({
         Counter: percent
     }, {
         duration: 1000,
-        step: function (now) {
-            small.text(Math.ceil(now) + '%');
+        step: function(now) {
+            $(this).text(Math.ceil(now) + '%');
         }
     });
 });
-  
